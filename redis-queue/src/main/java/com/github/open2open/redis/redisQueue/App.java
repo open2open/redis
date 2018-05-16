3c31525429adc4699b07fed4f190d8a34d0066f5
xiaopeng <work@xiaopeng.com>
2018-05-16 15:54:34 +0800

    redis queue

---
+++ redis-queue/src/main/java/com/github/open2open/redis/redisQueue/App.java
@@ -0,0 +1,13 @@
+package com.github.open2open.redis.redisQueue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
