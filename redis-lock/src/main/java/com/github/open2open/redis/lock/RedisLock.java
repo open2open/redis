cf580a715c7977de704c36e2952b221efeea1ddb
xiaopeng <work@xiaopeng.com>
2018-05-18 16:03:25 +0800

    注释

--- redis-lock/src/main/java/com/github/open2open/redis/lock/RedisLock.java
+++ redis-lock/src/main/java/com/github/open2open/redis/lock/RedisLock.java
@@ -36,6 +36,8 @@ public class RedisLock {
 		boolean flag = false;
 		String value = valueTreadLocal.get();
 		String key = value.split(":")[0];
+		//为什么不用del方法解锁,因为如果一个线程获取到锁了之后,如果操作业务的时间较长,在解锁的时候,可能这个线程获取到的锁过期了,它解锁的话可能把其它线程获取到的锁给解了
+		//用竞争锁并且将锁的过期时间设为0的方式是让锁自然过期
 		String setnx = jedis.set(key, value, "NX", "EX", 0);
 		valueTreadLocal.remove();
 		if(setnx.equals("OK")){
